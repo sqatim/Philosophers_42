eea84f8b54ee7b39364aba68077f6243d0499464
sqatim <sqatim@student.1337.ma>
2021-04-29 17:01:55 +0000

    fixing error in e;s;t

--- srcs_philo_one/philo_one.c
+++ srcs_philo_one/philo_one.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo_one.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ragegodthor <ragegodthor@student.42.fr>    +#+  +:+       +#+        */
+/*   By: sqatim <sqatim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/22 14:43:16 by sqatim            #+#    #+#             */
-/*   Updated: 2021/04/29 04:26:33 by ragegodthor      ###   ########.fr       */
+/*   Updated: 2021/04/29 16:37:48 by sqatim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,37 +75,17 @@ t_data get_args(int ac, char **av)
 
 	i = 1;
 	data.nb_of_philo = ft_atoi(av[1]);
-	// philo.forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * philo.number_of_philosopher);
-	// pthread_mutex_init(&mutex, NULL);
-	// pthread_mutex_init(&mutex1, NULL);
-	// pthread_mutex_init(&mutex2, NULL);
-	// pthread_mutex_init(&mutex3, NULL);
-
-	// philo.i = 0;
 	data.philo = (t_philo *)malloc(sizeof(t_philo) * data.nb_of_philo);
 	data.philo[0].fork = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * data.nb_of_philo);
 	pthread_mutex_init(&data.philo[0].fork[0], NULL);
 	while (i < data.nb_of_philo)
 	{
+		pthread_mutex_init(&data.philo[0].fork[i], NULL);
 		data.philo[i].fork = data.philo[0].fork;
+		// printf("adress fork[%d] ==> %p|\t|adress fork[0] ==> %p\n", i,data.philo[i].fork, data.philo[0].fork);
 		i++;
 	}
-	// while (i < data.nb_of_philo)
-	// {
-	// pthread_mutex_init(&data.philo[i].fork, NULL);
-	// i++;
-	// }
-	// philo.state_forks = (int *)malloc(sizeof(int) * philo.number_of_philosopher);
-	// philo.state_philo = (int *)malloc(sizeof(int) * philo.number_of_philosopher);
 	i = 0;
-	// while (i < data.nb_of_philo)
-	// {
-	// 	data.philo[i].j = 0;
-	// 	// data.philo[i].state = HUNGER;
-	// 	// data.philo[i].s_fork = EMPTY;
-	// 	i++;
-	// 	// philo.state_forks[i++] = EMPTY;
-	// }
 	while (i < data.nb_of_philo)
 	{
 		data.philo[i].nb_of_philo = ft_atoi(av[1]);
@@ -156,55 +136,24 @@ pthread_mutex_t *create_forks(int number, int *check)
 }
 void *routine(void *philosopher)
 {
-	pthread_mutex_t mutex;
-	pthread_mutex_t mutex1;
-
 	t_philo *philo;
 	// static int check;
 	int i;
 	// static pthread_mutex_t *fork;
 
-	pthread_mutex_init(&mutex, NULL);
-	pthread_mutex_init(&mutex1, NULL);
-	// check = 1;
 	philo = (t_philo *)philosopher;
 	i = 0;
-	// pthread_mutex_lock(&mutex);
-	// usleep(20);
-	// if (check == 1)
-	// {
-	// ++check;
-	// fork = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * philo->nb_of_philo);
-	// while (i < philo->nb_of_philo)
-	// {
-	// pthread_mutex_init(&fork[i], NULL);
-	// i++;
-	// }
-	// fork = create_forks(philo->nb_of_philo, &check);
-	// }
-	// printf("adress ==> %p\n", fork);
-	// puts("samir");
-	// pthread_mutex_unlock(&mutex);
 	while (1)
 	{
-		// puts("hii");
-		// puts("hi");
-		// pthread_mutex_lock(&mutex);
-		// usleep(20);
+		pthread_mutex_lock(&philo->fork[philo->r]);
+		printf("philo %d take the right fork[%d]\n", philo->l, philo->r);
 		pthread_mutex_lock(&philo->fork[philo->l]);
 		printf("philo %d take the left fork[%d]\n", philo->l, philo->l);
-		pthread_mutex_lock(&philo->fork[philo->r]);
-		// pthread_mutex_unlock(&mutex);
-		pthread_mutex_lock(&mutex1);
-		printf("philo %d take the right fork[%d]\n", philo->l, philo->r);
 		printf("philo %d start eating\n", philo->l);
-		pthread_mutex_unlock(&mutex1);
 		usleep(philo->time_to_eat * 1000);
-		pthread_mutex_lock(&mutex1);
 		printf("philo %d put forks\n", philo->l);
-		pthread_mutex_unlock(&mutex1);
-		pthread_mutex_unlock(&philo->fork[philo->l]);
 		pthread_mutex_unlock(&philo->fork[philo->r]);
+		pthread_mutex_unlock(&philo->fork[philo->l]);
 		printf("philo %d start sleeping\n", philo->l);
 		usleep(philo->time_to_sleep * 1000);
 		printf("philo %d start thinking\n", philo->l);
@@ -225,19 +174,14 @@ int main(int ac, char **av)
 	check_arguments(ac, av);
 	data = get_args(ac, av);
 	thread = (pthread_t *)malloc(sizeof(pthread_t) * data.nb_of_philo);
-	// while (i < data.philo->nb_of_philo)
-	// {
-	// pthread_mutex_init(&data.philo[i].fork, NULL);
-	// i++;
-	// }
 	i = 0;
 	while (i < data.nb_of_philo)
 	{
 		data.philo[i].l = i;
 		data.philo[i].r = (i + data.nb_of_philo - 1) % data.nb_of_philo;
 		pthread_create(&thread[i], NULL, &routine, (void *)&data.philo[i]);
-		usleep(100);
 		pthread_detach(thread[i]);
+		usleep(20);
 		i++;
 	}
 	while (1)
